64ffada03d51a24482966fe4c6e607e800622507
Ana Rebelo <anagmrebelo@hotmail.com>
2023-10-05 16:08:15 +0200

    CPP_06 ex02 invalid arg

--- cpp_06/ex02/srcs/functions.cpp
+++ cpp_06/ex02/srcs/functions.cpp
@@ -31,6 +31,8 @@ void	identify(Base* p)
 		std::cout << "B" << std::endl;
 	else if (dynamic_cast<C *>(p))
 		std::cout << "C" << std::endl;
+	else 
+		std::cout << "Not valid arg" << std::endl;
 }
 
 void	identify(Base& p)
@@ -60,4 +62,5 @@ void	identify(Base& p)
 	} catch (std::exception &e) {
 		;
 	}
+	std::cout << "Not valid arg" << std::endl;
 }
