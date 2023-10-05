baa293efe5f1975232b2d80aada4bb57a1ef21c4
Ana Rebelo <anagmrebelo@hotmail.com>
2023-10-05 18:32:05 +0200

    Create invalid tests files

---
+++ cpp_09/ex00/inc/BitcoinExchange.hpp
@@ -0,0 +1,11 @@
+#ifndef __BITCOINEXCHANGE_HPP__
+# define __BITCOINEXCHANGE_HPP__
+
+class BitcoinExchange
+{
+public:
+
+private:
+};
+
+# endif
---
+++ cpp_09/ex00/srcs/main.cpp
@@ -0,0 +1,17 @@
+int main (int argc, char *argv)
+{
+	if (isInvalidArgs(argc, argv))
+		return (1);
+
+	// Read data.csv and save into map
+		// If invalid db then error
+			// invalid means only or column or more than 2
+			// first string is not valid date
+			// second string is not float
+			// delimeter is not ','
+
+	// Read input file line by line
+		// print line by line 
+		// error for bad dates, non positive nb, bigger than 1000, delimeter not |
+	return (0);
+}
