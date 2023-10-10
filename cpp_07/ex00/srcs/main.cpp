e342f3a1f89b96050ca11dfa2771dfab26cac0e1
Ana Rebelo <anagmrebelo@hotmail.com>
2023-10-10 16:39:47 +0200

    CPP07 - Include main.cpp from eval

--- cpp_07/ex00/srcs/main.cpp
+++ cpp_07/ex00/srcs/main.cpp
@@ -1,27 +1,55 @@
 #include <iostream>
-#include "whatever.hpp"
+#include "Whatever.hpp"
 
-int	main( void )
+class Awesome
 {
-	int	a = 2;
-	int	b = 3;
+  public:
+    Awesome(void) : _n(0) {}
+    Awesome( int n ) : _n( n ) {}
+    Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
+    bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+    bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
+    bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+    bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+    bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+    bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+    int get_n() const { return _n; }
+  private:
+    int _n;
+};
+std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
 
-	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min(a, b) = " << ::min( a, b ) << std::endl;
-	std::cout << "max(a, b) = " << ::max( a, b ) << std::endl;
+int main(void)
+{
+	{
+		int	a = 2;
+		int	b = 3;
 
-	std::string	c = "chaine1";
-	std::string	d = "chaine2";
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min(a, b) = " << ::min( a, b ) << std::endl;
+		std::cout << "max(a, b) = " << ::max( a, b ) << std::endl;
 
-	::swap( c, d );
-	std::cout << "min(c, d) = " << ::min( c, d ) << std::endl;
-	std::cout << "max(c, d) = " << ::max( c, d ) << std::endl;
+		std::string	c = "chaine1";
+		std::string	d = "chaine2";
 
-	//::swap( c, d );
-	int	e = 3;
-	std::cout << "min(a, e) = " << ::min( a, e ) << std::endl;
-	std::cout << "min(NULL, NULL) = " << ::min( NULL, NULL ) << std::endl;
+		::swap( c, d );
+		std::cout << "min(c, d) = " << ::min( c, d ) << std::endl;
+		std::cout << "max(c, d) = " << ::max( c, d ) << std::endl;
+
+		//::swap( c, d );
+		int	e = 3;
+		std::cout << "min(a, e) = " << ::min( a, e ) << std::endl;
+		std::cout << "min(NULL, NULL) = " << ::min( NULL, NULL ) << std::endl;
+	}
 	
+	{
+		Awesome a(2), b(4);
+
+        swap(a, b);
+        std::cout << a << " " << b << std::endl;
+        std::cout << max(a, b) << std::endl;
+        std::cout << min(a, b) << std::endl;
+	}
 	return (0);
 }
