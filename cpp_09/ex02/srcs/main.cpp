477647df69e8d6995c4e299cd0688572f0f33505
arebelo <anagmrebelo@hotmail.com>
2023-11-10 19:04:14 +0100

    CPP_09 - ex02 arguments larger than 0

--- cpp_09/ex02/srcs/main.cpp
+++ cpp_09/ex02/srcs/main.cpp
@@ -1,32 +1,41 @@
 #include "PmergeMe.hpp"
 
-bool isOnlyDigits(char *str) {
-	for (unsigned long i = 0; str[i]; i++) {
+bool isOnlyDigits(char *str)
+{
+	for (unsigned long i = 0; str[i]; i++)
+	{
 		if (!isdigit(str[i]))
 			return (false);
 	}
 	return (true);
 }
 
-void validateArgs(int argc, char **argv) {
-	
+void validateArgs(int argc, char **argv)
+{
+
 	if (argc < 2)
 		throw PmergeMe::Error();
-	for (int i = 1; argv[i]; i++) {
+	for (int i = 1; argv[i]; i++)
+	{
 		if (!isOnlyDigits(argv[i]))
 			throw PmergeMe::Error();
-		try {
-			if (atof(argv[i]) > std::numeric_limits<int>::max())
+		try
+		{
+			if (atof(argv[i]) > std::numeric_limits<int>::max() || atof(argv[i]) == 0)
 				throw PmergeMe::Error();
-		} catch (std::exception & e) {
+		}
+		catch (std::exception &e)
+		{
 			throw PmergeMe::Error();
 		}
 	}
-	return ;
+	return;
 }
 
-int main( int argc, char **argv) {
-	try {
+int main(int argc, char **argv)
+{
+	try
+	{
 		std::vector<int> vector;
 		std::list<int> list;
 		std::vector<int> vectorSorted;
@@ -44,7 +53,7 @@ int main( int argc, char **argv) {
 			clock_t stop = clock();
 			durationVec = static_cast<double>(stop - start) / CLOCKS_PER_SEC * 1000;
 		}
-		
+
 		{
 			clock_t start = clock();
 			list = PmergeMe::createList(argv);
@@ -58,11 +67,10 @@ int main( int argc, char **argv) {
 		std::cout << "After - list: " << PmergeMe::printList(listSorted) << std::endl;
 		std::cout << "Time to process a range of " << vector.size() << " elements with std::vector: " << std::fixed << std::setprecision(5) << durationVec << " miliseconds" << std::endl;
 		std::cout << "Time to process a range of " << list.size() << " elements with std::list: " << std::fixed << std::setprecision(5) << durationList << " miliseconds" << std::endl;
-
-
-	} catch (std::exception & e) {
+	}
+	catch (std::exception &e)
+	{
 		std::cout << e.what() << std::endl;
 	}
 	return (0);
 }
-
