f1f5fb22580eb4b658b799f04b695d5e336511e0
arebelo <anagmrebelo@hotmail.com>
2023-11-10 17:46:56 +0100

    CPP_09 - Correct segfault when argc = 1

--- cpp_09/ex00/srcs/main.cpp
+++ cpp_09/ex00/srcs/main.cpp
@@ -7,22 +7,25 @@ static void validateArgs(int argc, char **argv)
 	std::fstream argFile;
 
 	if (argc != 2)
-		throw BitcoinExchange::CannotOpenFile(argv[1]);
+		throw BitcoinExchange::CannotOpenFile("");
 
 	argFile.open(argv[1]);
 	if (argFile.fail())
 		throw BitcoinExchange::CannotOpenFile(argv[1]);
-	
+
 	argFile.close();
 }
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
-	try {
+	try
+	{
 		validateArgs(argc, argv);
 		BitcoinExchange bitcoinExchange = BitcoinExchange("./srcs/data.csv", argv[1]);
 		bitcoinExchange.printConversions();
-	} catch (std::exception &e){
+	}
+	catch (std::exception &e)
+	{
 		std::cout << e.what() << std::endl;
 		return (1);
 	}
